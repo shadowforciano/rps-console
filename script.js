bb00daa27c9827634d72640d59229d5e5955c1cc
Shadowforciano <h.a.felicianojimenez@gmail.com>
2025-08-09 14:55:39 -0400

    feat: add getComputerChoice() using Math.random

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+console.log("RPS loaded");
+
+function getComputerChoice() {
+  const r = Math.random();      // r is in [0, 1)
+  if (r < 1/3) return "rock";
+  if (r < 2/3) return "paper";
+  return "scissors";
+}
+
+// quick test
+console.log("Computer says:", getComputerChoice());
